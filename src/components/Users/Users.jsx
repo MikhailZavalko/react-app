adf45bfd91383088cd872f431f33570f8df1558e
Mikhail Zavalko <mikhailzavalko@gmail.com>
2020-10-25 21:23:02 +1000

    after 62 lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,3 +1,4 @@
+import * as axios from 'axios';
 import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../assets/images/user.png';
@@ -11,6 +12,8 @@ let Users = (props) => {
 	for (let i = 1; i <= pagesCount; i++) {
 		pages.push(i);
 	}
+
+
 
 	return <div>
 		<div>
@@ -31,8 +34,37 @@ let Users = (props) => {
 					</div>
 					<div>
 						{u.followed
-							? <button onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-							: <button onClick={() => { props.follow(u.id) }}>Follow</button>}
+							? <button onClick={() => {
+
+								axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+									withCredentials: true,
+									headers: {
+										"API-KEY" : "a1deb2e2-7c97-4c64-b5cf-ee0c51f46b17"
+									}
+								})
+									.then(response => {
+										if (response.data.resultCode == 0) {
+											props.unfollow(u.id)
+										};
+									});
+
+							}}>Unfollow</button>
+							: <button onClick={() => {
+
+								axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+									withCredentials: true,
+									headers: {
+										"API-KEY": "a1deb2e2-7c97-4c64-b5cf-ee0c51f46b17"
+									}
+								})
+								.then(response => {
+									if(response.data.resultCode == 0) {
+										props.follow(u.id)
+									}
+								});
+
+								}}>Follow</button>}
+
 					</div>
 				</span>
 				<span>
