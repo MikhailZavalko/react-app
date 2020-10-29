e39b32294b4632c7f08d00bdbb098ad9de77cc6f
Mikhail Zavalko <mikhailzavalko@gmail.com>
2020-10-29 15:42:45 +1000

    after 73 lesson

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -18,7 +18,7 @@ const ProfileInfo = (props) => {
 
 				<img src={props.profile.photos.large} />
 
-				<ProfileStatus status={'hello my friends'} />
+				<ProfileStatus status={props.status} updateStatus={props.updateStatus} />
     	</div>
 		</div>
 	)
--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -2,19 +2,27 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 
 class ProfileStatus extends React.Component {
+
 	state = {
-		editMode: false
+		editMode: false,
 	}
 
 	activateEditMode = () => {
 		this.setState({
-			editMode: true
+			editMode: true,
+			status: this.props.status
 		})
 	}
 	deactivateEditMode = () => {
 		this.setState({
 			editMode: false
 		})
+		this.props.updateStatus(this.state.status);
+	}
+	onStatusChange = (e) => {
+		this.setState({
+			status: e.currentTarget.value
+		});
 	}
 
 	render() {
@@ -22,12 +30,12 @@ class ProfileStatus extends React.Component {
 			<div>
 				{!this.state.editMode &&
 					<div>
-						<span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
+						<span onDoubleClick={ this.activateEditMode }>{this.props.status || "---" }</span>
 					</div>
 				}
 				{this.state.editMode &&
 					<div>
-						<input autoFocus={true} onBlur={ this.deactivateEditMode } value={this.props.status} />
+						<input onChange={this.onStatusChange} autoFocus={true} onBlur={ this.deactivateEditMode } value={this.props.status} />
 					</div>
 				}
 			</div>
