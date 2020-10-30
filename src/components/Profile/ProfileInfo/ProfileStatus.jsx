5d85994641b9dd5fe3925c8f3fbe60e04c272904
Mikhail Zavalko <mikhailzavalko@gmail.com>
2020-10-30 11:44:12 +1000

    after 74 lesson, working bad

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -25,17 +25,25 @@ class ProfileStatus extends React.Component {
 		});
 	}
 
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.status !== this.props.status) {
+			this.setState({
+				status: this.props.status
+			});
+		}
+	}
+
 	render() {
 		return (
 			<div>
 				{!this.state.editMode &&
 					<div>
-						<span onDoubleClick={ this.activateEditMode }>{this.props.status || "---" }</span>
+						<span onDoubleClick={this.activateEditMode}>{this.props.status || "---"}</span>
 					</div>
 				}
 				{this.state.editMode &&
 					<div>
-						<input onChange={this.onStatusChange} autoFocus={true} onBlur={ this.deactivateEditMode } value={this.props.status} />
+						<input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode} value={this.props.status} />
 					</div>
 				}
 			</div>
