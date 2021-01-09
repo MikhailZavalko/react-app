8294fd9fe4057c818df051fe1ea5beba67b19a2f
Mikhail Zavalko <mikhailzavalko@gmail.com>
2021-01-09 21:43:13 +1000

    Updates

---
+++ precache-manifest.6d37c7c9e094362377e1e7cf4d6deebe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4a8e4f3152eeb60d2f128c68123ba6",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "8fa9e4a4412563839db6",
+    "url": "/react-app/static/css/3.a922c120.chunk.css"
+  },
+  {
+    "revision": "c49ac2edca0a1319339c",
+    "url": "/react-app/static/css/4.b27eef8c.chunk.css"
+  },
+  {
+    "revision": "93460cfa9cdb97fea0dd",
+    "url": "/react-app/static/css/main.4b05b185.chunk.css"
+  },
+  {
+    "revision": "0cca40822f4ca36bbfdb",
+    "url": "/react-app/static/js/2.cc9c085e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.cc9c085e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fa9e4a4412563839db6",
+    "url": "/react-app/static/js/3.16690a12.chunk.js"
+  },
+  {
+    "revision": "c49ac2edca0a1319339c",
+    "url": "/react-app/static/js/4.3f051c77.chunk.js"
+  },
+  {
+    "revision": "93460cfa9cdb97fea0dd",
+    "url": "/react-app/static/js/main.421bdf0a.chunk.js"
+  },
+  {
+    "revision": "4440721c75bf4f87948e",
+    "url": "/react-app/static/js/runtime-main.a726faa5.js"
+  },
+  {
+    "revision": "83f256d8e11c847a2b0d8f686091be55",
+    "url": "/react-app/static/media/upload.83f256d8.svg"
+  },
+  {
+    "revision": "600a2c9659fa8c55cd78465420d581c5",
+    "url": "/react-app/static/media/user.600a2c96.png"
+  }
+]);
